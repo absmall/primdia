a09a5e4c706735b13701ebba456e3bf501ea1643
Aaron Small <a.small@unb.ca>
2013-11-23 15:52:45 -0500

    Fix warnings about deprecated functions

--- gtk/src/GtkColor.cc
+++ gtk/src/GtkColor.cc
@@ -33,21 +33,18 @@ public:
 void GtkColorType::input(View *v, Binding *b) const
 {
 	GtkView *gtkv = static_cast<GtkView *>(v);
-	GtkWidget *dialog = gtk_color_selection_dialog_new(_("Select color"));
-	GtkColorSelection *colorsel = GTK_COLOR_SELECTION(gtk_color_selection_dialog_get_color_selection(GTK_COLOR_SELECTION_DIALOG(dialog)));
-	gtk_color_selection_set_has_opacity_control(colorsel, true);
-
+	GtkWidget *dialog = gtk_color_chooser_dialog_new(_("Select color"), GTK_WINDOW(gtkv->window));
 	gint ret = gtk_dialog_run(GTK_DIALOG(dialog));
 
 	if (ret == GTK_RESPONSE_OK)
 	{
-		GdkColor color;
-		double alpha = gtk_color_selection_get_current_alpha(colorsel);
-		gtk_color_selection_get_current_color(colorsel, &color);
-		if (!b->setValue(gtkv->document, new Color((unsigned char)scale_round(color.red/65535.0, 255),
-						(unsigned char)scale_round(color.green/65535.0,255),
-						(unsigned char)scale_round(color.blue/65535.0,255),
-						(unsigned char)scale_round(alpha/65535.0,255))))
+        GdkRGBA rgba;
+        gtk_color_chooser_get_rgba(GTK_COLOR_CHOOSER(dialog), &rgba);
+
+		if (!b->setValue(gtkv->document, new Color((unsigned char)scale_round(rgba.red, 255),
+						(unsigned char)scale_round(rgba.green,255),
+						(unsigned char)scale_round(rgba.blue,255),
+						(unsigned char)scale_round(rgba.alpha,255))))
 		{
 			GtkWidget *dialog = gtk_message_dialog_new(NULL, GTK_DIALOG_MODAL, GTK_MESSAGE_ERROR, GTK_BUTTONS_CLOSE, _("The binding could not be set. It overconstrains an object in the diagram."));
 			g_signal_connect_swapped (dialog, "response", G_CALLBACK (gtk_widget_destroy), dialog);
